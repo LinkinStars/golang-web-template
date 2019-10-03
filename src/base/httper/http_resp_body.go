e2c49dbe269f69078c797b5cecee880786fbd510
LinkinStar <linkinstar@foxmail.com>
2019-10-03 17:03:12 +0800

    请求返回结构修改完成

--- src/base/httper/http_resp_body.go
+++ src/base/httper/http_resp_body.go
@@ -6,28 +6,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Response 通用返回模型
 type Response struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data,omitempty"`
 }
 
-func SendResponse(c *gin.Context, code int, message string, data interface{}) {
-	c.JSON(http.StatusOK, Response{
+// SendResp 返回自定义数据
+func SendResp(c *gin.Context, httpCode, code int, message string, data interface{}) {
+	c.JSON(httpCode, Response{
 		Code:    code,
 		Message: message,
 		Data:    data,
 	})
 }
 
-func SendResponseFail(c *gin.Context, httpCode int, message string) {
+// SendFailResp 返回失败请求
+func SendFailResp(c *gin.Context, httpCode int, message string) {
 	c.JSON(httpCode, Response{
 		Code:    -1,
 		Message: message,
 	})
 }
 
-func SendResponseSuccess(c *gin.Context, message string, data interface{}) {
+// SendSuccessResp 返回成功请求
+func SendSuccessResp(c *gin.Context, message string, data interface{}) {
 	c.JSON(http.StatusOK, Response{
 		Code:    1,
 		Message: message,
