440b765e1b1033364e6d3deff0ae259a7145d9ae
LinkinStar <linkinstar@foxmail.com>
2019-10-03 17:30:46 +0800

    用户模型修改完成

--- src/model/user_model.go
+++ src/model/user_model.go
@@ -1,8 +1,9 @@
 package model
 
+// User 用户
 type User struct {
-	Id       int    `xorm:"not null pk autoincr comment('用户id') INT(11)"`
-	Username string `xorm:"not null comment('用户名') VARCHAR(32)"`
-	Nickname string `xorm:"not null comment('昵称') VARCHAR(32)"`
-	Age      int    `xorm:"not null comment('年龄') INT(11)"`
+	ID       int    `xorm:"not null pk autoincr comment('用户id') INT(11) id"`
+	Username string `xorm:"not null comment('用户名') VARCHAR(32) username"`
+	Nickname string `xorm:"comment('昵称') VARCHAR(32) nickname"`
+	Age      int    `xorm:"not null comment('年龄') INT(11) age"`
 }
